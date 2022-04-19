0b2ea87eba6004951ae37b84e5dd68c486058248
bluuweb <contacto@bluuweb.cl>
2022-04-19 17:47:19 -0400

    fish

--- README.md
+++ README.md
@@ -27,3 +27,32 @@ Ver y eliminar git remote
 git remote -v
 git remote rm heroku
 ```
+
+## Hash password
+
+-   [doc](https://x-team.com/blog/storing-secure-passwords-with-postgresql/)
+
+```sql
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS users;
+
+CREATE EXTENSION pgcrypto;
+
+CREATE TABLE IF NOT EXISTS users (
+	uid SERIAL PRIMARY KEY,
+	username VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	password VARCHAR(255) NOT NULL,
+	role VARCHAR(100) NOT NULL DEFAULT 'customer'
+);
+
+CREATE TABLE IF NOT EXISTS books (
+	name VARCHAR(255) NOT NULL,
+	uid INT REFERENCES users(uid)
+);
+
+INSERT INTO users (username, email, password, role)
+VALUES ('admin', 'admin@test.com', crypt('123123', gen_salt('bf')), 'admin');
+INSERT INTO users (username, email, password)
+VALUES ('dexter', 'dexter@test.com', crypt('321321', gen_salt('bf')));
+```
--- database/db.js
+++ database/db.js
@@ -9,13 +9,15 @@ const pool = new Pool(
     }
 );
 
+// https://x-team.com/blog/storing-secure-passwords-with-postgresql/
 const migrate = async () => {
     try {
         const data = fs.readFileSync(__dirname + "/users.sql", {
             encoding: "utf-8",
         });
+        // console.log(data);
         await pool.query(data);
-        console.log("tabla creada");
+        console.log("tablas creadas");
     } catch (error) {
         console.log(error);
     }
@@ -23,7 +25,20 @@ const migrate = async () => {
 
 const users = async () => pool.query("SELECT * FROM users;");
 
+const pruebaLogin = async () => {
+    try {
+        const data = await pool.query(
+            "SELECT * FROM users WHERE email = $1 AND password = crypt($2, password);",
+            ["admin@test.com", "123123"]
+        );
+        console.log(data.rows);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 module.exports = {
     migrate,
     users,
+    pruebaLogin,
 };
